03fca0db72681782d3084f27587645769b397f3f
jaibhim bangrey <jaibhimb@gmail.com>
2025-07-07 11:54:14 +0530

    update in forntend project

--- src/main.jsx
+++ src/main.jsx
@@ -6,29 +6,34 @@ import { store } from "./App/Store.js";
 import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Home          from "./components/Home.jsx";
-import About         from "./components/About.jsx";
-import Contact       from "./components/Contact.jsx";
-import Product     from "./components/Product.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import Product from "./components/Product.jsx";
 import SingleProduct from "./components/SingleProduct.jsx";
-import Cart          from "./components/Cart.jsx";
-import Footer        from "./components/Footer.jsx";
+import Cart from "./components/Cart.jsx";
+import Footer from "./components/Footer.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "about", element: <About /> },
-      { path: "contact", element: <Contact /> },
-      { path: "product", element: <Product /> },
-      { path: "singleProduct/:id", element: <SingleProduct /> },
-      { path: "cart", element: <Cart /> },
-      { path: "footer", element: <Footer /> }
-    ]
-  }
-]);
+//
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "contact", element: <Contact /> },
+        { path: "product", element: <Product /> },
+        { path: "singleProduct/:id", element: <SingleProduct /> },
+        { path: "cart", element: <Cart /> },
+        { path: "footer", element: <Footer /> },
+      ],
+    },
+  ],
+  { basename: "/ecomm" }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
